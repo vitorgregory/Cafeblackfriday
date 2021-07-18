964a6f5a0451feaa1c37d894b6538b5b5134408f
vitorgregory <vitor.gregory.lins@gmail.com>
2021-07-18 19:04:45 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+const form = document.getElementById('form')
+
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+  let email = document.getElementById('email').value;
+  let data = {
+    email,
+  }
+  let convertData = JSON.stringify(data);
+
+  localStorage.setItem('lead', convertData)
+
+  let content = document.getElementById('content')
+
+})
