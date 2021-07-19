836e3619542bd6887b021907034700c3344a50a9
vitorgregory <vitor.gregory.lins@gmail.com>
2021-07-19 13:35:31 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,13 +1,14 @@
 const form = document.getElementById('form')
+const botao = document.getElementById('botao')
 
-form.addEventListener('submit', (e) => {
+form.addEventListener('click', (e) => {
   e.preventDefault();
   let email = document.getElementById('email').value;
   let data = {
     email,
   }
   let convertData = JSON.stringify(data);
-
+  console.log('data', convertData)
   localStorage.setItem('lead', convertData)
 
   let content = document.getElementById('content')
